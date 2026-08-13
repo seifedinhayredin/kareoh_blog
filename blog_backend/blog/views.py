f257c860f1719250a78ec9161bfeca17d135041f
seifedinhayredin <seifedinhayredin@gmail.com>
2026-08-13 07:52:46 +0300

    mine api

--- blog_backend/blog/views.py
+++ blog_backend/blog/views.py
@@ -1,4 +1,7 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Post
 from .serializers import PostSerializer
@@ -6,9 +9,8 @@ from .permissions import IsAuthorOrReadOnly
 from .utils import generate_unique_slug
 
 
-class PostViewSet(ModelViewSet):
 
-    queryset = Post.objects.all()
+class PostViewSet(ModelViewSet):
 
     serializer_class = PostSerializer
 
@@ -17,6 +19,24 @@ class PostViewSet(ModelViewSet):
     ]
 
     lookup_field = "slug"
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # Anonymous users can only see published posts
+        if not user.is_authenticated:
+            return Post.objects.filter(
+                status=Post.Status.PUBLISHED
+            )
+
+        # Authenticated users can see:
+        # published posts + their own drafts
+        return Post.objects.filter(
+            status=Post.Status.PUBLISHED
+        ) | Post.objects.filter(
+            author=user,
+            status=Post.Status.DRAFT
+        )
 
     def perform_create(self, serializer):
 
@@ -30,4 +50,25 @@ class PostViewSet(ModelViewSet):
         serializer.save(
             author=self.request.user,
             slug=slug
+        )
+
+    @action(
+        detail=False,
+        methods=["get"],
+        url_path="mine",
+        permission_classes=[IsAuthenticated],
+    )
+    def my_posts(self, request):
+
+        posts = Post.objects.filter(
+            author=request.user
+        )
+
+        serializer = self.get_serializer(
+            posts,
+            many=True
+        )
+
+        return Response(
+            serializer.data
         )
