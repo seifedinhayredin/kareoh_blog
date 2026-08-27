31c2fa59954276e3e03edd43e4323052f1f56935
seifedinhayredin <seifedinhayredin@gmail.com>
2026-08-27 06:57:24 +0300

    updated pagination

--- blog_backend/config/settings.py
+++ blog_backend/config/settings.py
@@ -147,6 +147,11 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.AllowAny",
     ),
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.PageNumberPagination"
+    ),
+
+    "PAGE_SIZE": 7,
 }
 
 from datetime import timedelta
